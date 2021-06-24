eb3e244aae5521f809fa6edc0f6cd881bc89a0a6
sanggoalist <s.tran@goalist.co.jp>
2021-06-24 16:54:27 +0700

    database setting

--- src/main/java/com/e_rental/owner/OwnerApplication.java
+++ src/main/java/com/e_rental/owner/OwnerApplication.java
@@ -1,9 +1,12 @@
 package com.e_rental.owner;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class OwnerApplication {
 
 	public static void main(String[] args) {
