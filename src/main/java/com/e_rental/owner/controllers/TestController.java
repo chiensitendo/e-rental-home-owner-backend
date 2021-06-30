7929fbf3c34fef615591a40436e33e2475706944
sanggoalist <s.tran@goalist.co.jp>
2021-06-30 15:27:02 +0700

    add test controllers

---
+++ src/main/java/com/e_rental/owner/controllers/TestController.java
@@ -0,0 +1,27 @@
+package com.e_rental.owner.controllers;
+
+import com.e_rental.owner.responses.UserListResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/users")
+    @ResponseBody
+    public ResponseEntity<List<String>> getAll(){
+        List<String> list = new ArrayList<String>();
+        list.add("Test 1");
+        list.add("Test 2");
+        return ResponseEntity.ok(list);
+    }
+}
